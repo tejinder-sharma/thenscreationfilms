55bfaf87444fb2af954940fe155c6750f9b8fc85
tejinder-sharma <tejinderwebdev@gmail.com>
2022-04-24 13:57:02 +0530

    initial commit

---
+++ public/NSmain.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".mySwiper", {
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {  
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+  });
